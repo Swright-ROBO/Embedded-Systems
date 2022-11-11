fe077a87c1367478970761f3a89af3927ec94000
Swright-ROBO <78348631+Swright-ROBO@users.noreply.github.com>
2022-11-11 16:26:19 +0000

    Attempted Task 370
    Will continue this weekend

--- Tasks/Task-364B-WatchDog/main.cpp
+++ Tasks/Task-364B-WatchDog/main.cpp
@@ -7,34 +7,45 @@ using namespace uop_msb;
 
 void task1();
 void task2();
+void task3();
 
-typedef enum {NONE=0, THREAD1=0b01, THREAD2=0b10, ALL=0b11} THREADHEALTH; 
-uint8_t threadHealth = 0b00;
+typedef enum {NONE=0, THREAD1=0b01, THREAD2=0b10, ALL=0b111, THREAD3=0b100} THREADHEALTH; 
+uint8_t threadHealth = 0b000;
 Mutex threadHealthLock;
 void isAlive(THREADHEALTH th)
 {
     threadHealthLock.lock();
     threadHealth |= th;
     if (threadHealth & THREAD1) {
-        printf("Thread 1 checked in\n");
+        Watchdog::get_instance().kick();
+        threadHealth = NONE;
+        printf("Thread 1 checked in: Reset Watchdog\n");
     }
     if (threadHealth & THREAD2) {
-        printf("Thread 2 checked in\n");
+        Watchdog::get_instance().kick();
+        threadHealth = NONE;
+        printf("Thread 2 checked in: Reset Watchdog\n");
+    }
+    if (threadHealth & THREAD3)
+    {
+        Watchdog::get_instance().kick();
+        threadHealth = NONE;
+        printf("Thread 3 checked in: Reset Watchdog\n");
     }
     if (threadHealth == ALL) {
         Watchdog::get_instance().kick();
         threadHealth = NONE;
-        printf("Both checked in: Reset Watchdog\n");
+        printf("All checked in: Reset Watchdog\n");
     }
     threadHealthLock.unlock();
 }
 
 //Threads
-Thread t1, t2;
+Thread t1, t2, t3;
 
 //Locks
-Mutex m1;
-Mutex m2;
+/*Mutex m1;
+Mutex m2;*/
 
 #define TIMEOUT_MS 10000
 int main() {
@@ -45,10 +56,12 @@ int main() {
 
     t1.start(task1);
     t2.start(task2);
+    t3.start(task3);
 
     //Wait for t1 and t2 to end (which they never do)
     t1.join();
     t2.join();
+    t3.join();
 }
 
 // Version 1 - Partially uses a spinning technique
@@ -60,20 +73,20 @@ void task1() {
     while(true) {
 
         while (sw1 == 0) {};            //BLOCKS via SPINNING
-        m1.lock();
+        //m1.lock();
         
         isAlive(THREAD1);
         ThisThread::sleep_for(50ms);    //Blocks in the WAITING state
 
-        m2.lock();      //For demo
+        //m2.lock();      //For demo
         while (sw1 == 1) {};            //BLOCKS via SPINNING
-        m2.unlock();    //For demo
+        //m2.unlock();    //For demo
 
         isAlive(THREAD1);
         red_led = !red_led;             
         ThisThread::sleep_for(50ms);    //Blocks in the WAITING state
 
-        m1.unlock();    //For demo
+        //m1.unlock();    //For demo
     }    
 }
 
@@ -86,21 +99,44 @@ void task2()
     while(true) {
 
         button2.waitForPress();         //Blocks in the WAITING state 
-        m2.lock();      //For demo
+        //m2.lock();      //For demo
 
         isAlive(THREAD2);
         ThisThread::sleep_for(50ms);    //Blocks in the WAITING state
 
-        m1.lock();
+        //m1.lock();
         button2.waitForRelease();       //Blocks in the WAITING state
-        m1.unlock();
+        //m1.unlock();
 
         isAlive(THREAD2);
         green_led = !green_led;         
         ThisThread::sleep_for(50ms);    //Blocks in the WAITING state
 
-        m2.unlock();
+        //m2.unlock();
     }    
 }
 
+void task3() 
+{
+    DigitalOut yellow_led(TRAF_YEL1_PIN);
+    PushSwitch button3(BTN3_PIN);
 
+    while(true) {
+
+        button3.waitForPress();         //Blocks in the WAITING state 
+        //m2.lock();      //For demo
+
+        isAlive(THREAD3);
+        ThisThread::sleep_for(50ms);    //Blocks in the WAITING state
+
+        //m1.lock();
+        button3.waitForRelease();       //Blocks in the WAITING state
+        //m1.unlock();
+
+        isAlive(THREAD3);
+        yellow_led = !yellow_led;         
+        ThisThread::sleep_for(50ms);    //Blocks in the WAITING state
+
+        //m2.unlock();
+    }    
+}
--- Tasks/Task-370-Challenge/main.cpp
+++ Tasks/Task-370-Challenge/main.cpp
@@ -4,7 +4,12 @@ using namespace uop_msb;
 
 //Used this to 
 Ticker tick;
-
+Thread t1;
+void task1();
+void ADCTrigger()
+{
+    t1.flags_set(1);
+}
 
 int main(void)
 {
@@ -14,8 +19,17 @@ int main(void)
 3. Each time it is unblocked by the signal, it should read the ADC (Use `AnalogIn`) for the LDR and add it to a running sum.
 4. Every 1s, it should print out the average of the past 1000 samples to the terminal. 
 */
+    t1.start(task1);
+    tick.attach(&ADCTrigger, 1ms);
     while (true) {
         ThisThread::sleep_for(1000ms);
     }
     
-}   
+}
+
+void task1()
+{
+    ThisThread::flags_wait_any(1);
+    printf("Recieved ping\n");
+    ThisThread::flags_clear(1);
+}
